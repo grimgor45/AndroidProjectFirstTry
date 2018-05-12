89b09eb947e3d032cb3306f1dd036a7595c331c1
HS <seb.frkl@hotmail.fr>
2018-05-12 16:14:09 +0200

    Added Toast correct answer and dialog quit quizz

--- app/src/main/java/projetmobile/esiea/quiz/MainActivity.java
+++ app/src/main/java/projetmobile/esiea/quiz/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,7 +19,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menumain, menu);
+
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                return true;
+            case R.id.action_settings:
+                return true;
+            case R.id.action_save:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @Override
--- app/src/main/java/projetmobile/esiea/quiz/Questions.java
+++ app/src/main/java/projetmobile/esiea/quiz/Questions.java
@@ -1,11 +1,15 @@
 package projetmobile.esiea.quiz;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,6 +27,8 @@ public class Questions extends AppCompatActivity {
     public AnswerList aL;
     public int score;
     public int actualScore;
+    JSONArray list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -33,24 +39,18 @@ public class Questions extends AppCompatActivity {
 
         GetBiersService.startActionGetAllBiers(Questions.this, null);
 
+        list = getBiersFromFile();
+
+        changeAnswerList(list);
+
+
         Button loadButton = (Button) findViewById(R.id.ButtonLoad);
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 JSONArray list = getBiersFromFile();
 
-                aL = AnswerList.getInstance(list);
-
-                try {
-                    ((TextView)findViewById(R.id.QuizzQuestion)).setText("Description de la bière "+aL.objList[aL.correct-1].getString("description"));
-                    ((Button)findViewById(R.id.Answer1)).setText(aL.objList[0].getString("name"));
-                    ((Button)findViewById(R.id.Answer2)).setText(aL.objList[1].getString("name"));
-                    ((Button)findViewById(R.id.Answer3)).setText(aL.objList[2].getString("name"));
-                    ((Button)findViewById(R.id.Answer4)).setText(aL.objList[3].getString("name"));
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                changeAnswerList(list);
             }
         });
 
@@ -61,10 +61,19 @@ public class Questions extends AppCompatActivity {
                 if (aL.correct ==1)
                 {
                     score++;
+                    Context context = getApplicationContext();
+                    CharSequence text = "Correct Answer";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 }
                 actualScore++;
                 ((TextView)findViewById(R.id.Score)).setText(((Integer)score).toString());
                 ((TextView)findViewById(R.id.ActualScore)).setText(((Integer)actualScore).toString());
+                changeAnswerList(list);
+
+
 
             }
         });
@@ -75,10 +84,17 @@ public class Questions extends AppCompatActivity {
                 if (aL.correct ==2)
                 {
                     score++;
+                    Context context = getApplicationContext();
+                    CharSequence text = "Correct Answer";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 }
                 actualScore++;
                 ((TextView)findViewById(R.id.Score)).setText(((Integer)score).toString());
                 ((TextView)findViewById(R.id.ActualScore)).setText(((Integer)actualScore).toString());
+                changeAnswerList(list);
 
 
             }
@@ -90,10 +106,17 @@ public class Questions extends AppCompatActivity {
                 if (aL.correct ==3)
                 {
                     score++;
+                    Context context = getApplicationContext();
+                    CharSequence text = "Correct Answer";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 }
                 actualScore++;
                 ((TextView)findViewById(R.id.Score)).setText(((Integer)score).toString());
                 ((TextView)findViewById(R.id.ActualScore)).setText(((Integer)actualScore).toString());
+                changeAnswerList(list);
 
 
             }
@@ -105,13 +128,51 @@ public class Questions extends AppCompatActivity {
                 if (aL.correct ==4)
                 {
                     score++;
+                    Context context = getApplicationContext();
+                    CharSequence text = "Correct Answer";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
                 }
                 actualScore++;
                 ((TextView)findViewById(R.id.Score)).setText(((Integer)score).toString());
                 ((TextView)findViewById(R.id.ActualScore)).setText(((Integer)actualScore).toString());
+                changeAnswerList(list);
 
             }
         });
+
+        Button quit = (Button)findViewById(R.id.Buttonquit);
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = TrulyExit();
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+
+    private AlertDialog.Builder TrulyExit(){
+        Context context = getApplicationContext();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+// Add the buttons
+        builder.setMessage("This will end the activity");
+
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        return builder;
+
     }
 
 
@@ -128,6 +189,21 @@ public class Questions extends AppCompatActivity {
                     return new JSONArray();
                 }
             }
+
+    private void changeAnswerList(JSONArray list){
+        aL = AnswerList.getInstance(list);
+
+        try {
+            ((TextView)findViewById(R.id.QuizzQuestion)).setText("Description de la bière "+aL.objList[aL.correct-1].getString("description"));
+            ((Button)findViewById(R.id.Answer1)).setText(aL.objList[0].getString("name"));
+            ((Button)findViewById(R.id.Answer2)).setText(aL.objList[1].getString("name"));
+            ((Button)findViewById(R.id.Answer3)).setText(aL.objList[2].getString("name"));
+            ((Button)findViewById(R.id.Answer4)).setText(aL.objList[3].getString("name"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+    }
+}
 
 
